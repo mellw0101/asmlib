b0d3c20ddde24288bbc2152a3cb51da278d2e3f6
mellw0101 <melwivensson@gmail.com>
2024-05-28 04:58:45 +0200

    hello

--- src/asm_malloc.asm
+++ src/asm_malloc.asm
@@ -1,23 +1,15 @@
 %include "/home/mellw/CLionProjects/asmlib/src/asm_macros.inc"
 section .text
     global asm_malloc
-
+; Description: Allocate memory using the brk system call
 asm_malloc:
-    ;
-    ; Description: Allocate memory using the brk system call
-    ; Function arguments
-    ; rdi: size of memory to allocate
-    ;
     __FUNC_START__
-    ; Save the requested size
-    mov rsi, rdi
+    mov rsi, rdi        ; Save the requested size
 
     ; Call sbrk(0) to get the current program break
     mov rax, 12         ; syscall number for brk
     xor rdi, rdi        ; NULL pointer to get current break
-    syscall
-
-    ; rax now contains the current program break (old_brk)
+    syscall             ; rax is the reg for return status
     mov rbx, rax        ; save the current break in rbx
 
     ; Calculate the new program break
@@ -26,9 +18,8 @@ asm_malloc:
     ; Call brk(new_brk) to set the new program break
     mov rax, 12         ; syscall number for brk
     mov rdi, rbx        ; new program break
-    syscall
+    syscall             ; rax = exit code from syscall
 
-    ; Check if brk failed
     cmp rax, rbx        ; compare ( rax == rbx )
     jne .alloc_fail     ; If not equal, allocation failed
 
@@ -37,7 +28,7 @@ asm_malloc:
     sub rax, rsi
     jmp .exit
 
-    .alloc_fail: ; Allocation failed, return NULL (0)
-        xor rax, rax
+    .alloc_fail:        ; Allocation failed
+        xor rax, rax    ; return NULL (0)
     .exit:
         __FUNC_END__
